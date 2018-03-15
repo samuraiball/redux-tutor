14013ea1b95a045c2f6444f117d5a60037e4f1e5
samuraiballhiro <yuy.hirohiro@gamil.com>
2018-03-15 18:53:27 +0900

    buggix

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import {createStore} from 'redux'
 import ReactDom from 'react-dom'
 import todoApp from './reducer/reducers'
 import App from './compornet/App'
-import { Provider } from 'react-redux'
+import {Provider} from 'react-redux'
 
 let store = createStore(todoApp)
 
 ReactDom.render(
-<Provider store={store}>
-    <App/>
-</Provider>,
+    <Provider store={store}>
+        <App/>
+    </Provider>,
     document.getElementById('root')
 )
